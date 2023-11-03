a209c7d79bdfaf7a0739fe9f3a210d115ee6815e
Saadat Nursultan <nursultan.saadat@gmail.com>
2023-11-03 20:08:44 +0100

    Smiley face

--- src/code/Tutorial.js
+++ src/code/Tutorial.js
@@ -8,21 +8,26 @@ export const CanvasTutorial = () => {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d');
 
+      // smiley face
       ctx.beginPath();
-      ctx.moveTo(75, 50);
-      ctx.lineTo(100, 75);
-      ctx.lineTo(100, 25);
-      ctx.fill();
+      // outer circle
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+      ctx.moveTo(110, 75);
+      // mouth
+      ctx.arc(75, 75, 35, 0, Math.PI, false);
+      ctx.moveTo(65, 65);
+      // left eye
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true);
+      ctx.moveTo(95, 65);
+      // right eye
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true);
+      ctx.stroke();
     }
   }
 
   useEffect(() => {
     draw();
   }, []);
-
-  // executed once the page has finished loading
-  // done by listening for the load event on the document
-  // window.addEventListener('load', draw);
 
   return <canvas height='150px' width='150px' ref={canvasRef}></canvas>;
 };
