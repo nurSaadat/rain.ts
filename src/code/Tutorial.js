3db9e01f81038bb648fc231403938b0719be0164
Saadat Nursultan <nursultan.saadat@gmail.com>
2023-11-03 19:57:28 +0100

    Drawing triangle

--- src/code/Tutorial.js
+++ src/code/Tutorial.js
@@ -8,9 +8,11 @@ export const CanvasTutorial = () => {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d');
 
-      ctx.fillRect(25, 25, 100, 100);
-      ctx.clearRect(45, 45, 60, 60);
-      ctx.strokeRect(50, 50, 50, 50);
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fill();
     }
   }
 
