7b31b745b22b1097fe373f57f08728ee1965bcd2
Saadat Nursultan <nursultan.saadat@gmail.com>
2023-11-04 22:12:56 +0100

    Two triangles

--- src/code/Tutorial.js
+++ src/code/Tutorial.js
@@ -8,25 +8,18 @@ export const CanvasTutorial = () => {
       const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d');
 
-      for (let i = 0; i <= 3; i++) {
-        for (let j = 0; j <= 2; j++) {
-          ctx.beginPath();
-          let x = 25 + j * 50; // x coordinate
-          let y = 25 + i * 50; // y coordinate
-          let radius = 20; // Arc radius
-          let startAngle = 0; // Starting point on circle
-          let endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
-          let counterclockwise = i % 2 === 1; // Draw counterclockwise
+      ctx.beginPath();
+      ctx.moveTo(25, 25);
+      ctx.lineTo(105, 25);
+      ctx.lineTo(25, 105);
+      ctx.fill();
 
-          ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
-
-          if (i > 1) {
-            ctx.fill();
-          } else {
-            ctx.stroke();
-          }
-        }
-      }
+      ctx.beginPath();
+      ctx.moveTo(125, 125);
+      ctx.lineTo(125, 45);
+      ctx.lineTo(45, 125);
+      ctx.closePath();
+      ctx.stroke();
     }
   }
 
